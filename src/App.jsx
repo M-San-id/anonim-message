75063ed74f76a326776e5de64bcaf0a1bd0b7826
Muhamad Ikhsan <muhamadikhsan.net@gmail.com>
2025-07-11 18:22:42 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import "./App.css";
+
+function App() {
+  const form = useRef();
+  const [status, setStatus] = useState("");
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setStatus("Sending...");
+
+    // Ganti dengan Service ID, Template ID, dan Public Key Anda dari EmailJS
+    const serviceId = "service_hwda72l";
+    const templateId = "template_7silzct";
+    const publicKey = "C6JlIiElk2rvlkiuO"; // Ini aman untuk diungkapkan di sisi klien
+
+    emailjs.sendForm(serviceId, templateId, form.current, publicKey).then(
+      (result) => {
+        console.log(result.text);
+        setStatus("Email sent successfully!");
+        form.current.reset(); // Reset form
+      },
+      (error) => {
+        console.log(error.text);
+        setStatus(`Failed to send email: ${error.text}`);
+      }
+    );
+  };
+
+  return (
+    <div>
+      <div className="form-container">
+        <form className="form" ref={form} onSubmit={sendEmail}>
+          <div className="form-group">
+            <label htmlFor="message" className="title">
+              Tulis pesan disini
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              rows="10"
+              cols="50"
+              required
+              placeholder="Tulis pesan Anda di sini..."
+            />
+          </div>
+          <button type="submit">Kirim</button>
+          {status && <p>{status}</p>}
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
